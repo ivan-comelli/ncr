8dd5c1e92f5667ecd2aff8c1df23f29cacd8a06e
Ivan Comelli <ivancomelli.99@gmail.com>
2025-07-10 12:38:11 -0300

    new

--- config-overrides.js
+++ config-overrides.js
@@ -2,28 +2,22 @@ const webpack = require('webpack');
 
 module.exports = function override(config) {
   config.resolve.fallback = {
-    ...config.resolve.fallback,
-    process: require.resolve('process/browser.js'),
-    stream: require.resolve('stream-browserify'),
-    zlib: require.resolve('browserify-zlib'),
     crypto: require.resolve('crypto-browserify'),
-    buffer: require.resolve('buffer'),
+    stream: require.resolve('stream-browserify'),
     path: require.resolve('path-browserify'),
     os: require.resolve('os-browserify/browser'),
     https: require.resolve('https-browserify'),
     http: require.resolve('stream-http'),
-    fs: false, // NO se puede polyfillear en navegador
-    net: false, // NO se puede polyfillear en navegador
-    tls: false  // NO se puede polyfillear en navegador
+    buffer: require.resolve('buffer'),
+    process: require.resolve('process/browser'),
   };
 
-  config.plugins = [
-    ...(config.plugins || []),
+  config.plugins.push(
     new webpack.ProvidePlugin({
-      process: 'process/browser.js',
       Buffer: ['buffer', 'Buffer'],
-    }),
-  ];
+      process: 'process/browser',
+    })
+  );
 
   return config;
 };
